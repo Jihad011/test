8e745cedf0c4dc11ac6b8efdebfdfec2f90d75b5
Jihad011 <126379673+Jihad011@users.noreply.github.com>
2023-08-28 06:12:54 -0400

    Add files via upload

---
+++ c++.c
@@ -0,0 +1,9 @@
+    #include<iostream>
+    using namespace std;
+    int main(){
+          int i=5;
+          double d=1.2;
+          char c='h';
+          cout<<i<<d<<c;
+          return 0;
+    }
